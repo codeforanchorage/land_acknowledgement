c39381b2209f99726f7cc92a9a826d2762e1680c
Mark Meyer <mark@photo-mark.com>
2020-06-09 19:20:37 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { getLandText } = require("./lib");
+
+const http = require('http');
+const express = require('express');
+const { urlencoded } = require('body-parser');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+app.use(urlencoded({ extended: false }));
+
+app.post('/sms', async (req, res) => {
+  const twiml = new MessagingResponse();
+  const message = req.body.Body;
+  const landText = await getLandText(message);
+ 
+  twiml.message(landText);
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(3001, () => {
+  console.log('Express server listening on port 3000');
+});
