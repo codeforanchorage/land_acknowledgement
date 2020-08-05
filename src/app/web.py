dce1cb682830588b3b8f90a9405f783b5436031f
Mark Meyer <mark@photo-mark.com>
2020-08-04 17:30:00 -0800

    refactor falcon code

--- src/app/web.py
+++ src/app/web.py
@@ -1,10 +1,14 @@
-from urllib.parse import parse_qs
 import falcon
 from twilio.twiml.messaging_response import MessagingResponse
 from .db import GeoData
-from .logger import logger
 
 
+def make_twilML(req, resp, resource):
+    twil_resp = MessagingResponse()
+    resp.body = str(twil_resp.message(resp.body))
+
+
+@falcon.after(make_twilML)
 class LandResource(object):
     def on_get(self, req, resp):
         resp.body = (
@@ -12,23 +16,11 @@ class LandResource(object):
         )
 
     def on_post(self, req, resp):
-        twil_resp = MessagingResponse()
-        resp.set_header('Content-Type', 'text/html; charset=UTF-8')
 
-        if req.content_length == 0:
-            twil_resp.message(f"Please tell me the town and state you are in. For example, 'Anchorage, AK'")
-            resp.body = str(twil_resp)
-            return
-
-        data = req.stream.read()
-        params = parse_qs(data.decode('utf-8'))
-        logger.info(params)
-
-        query = params['Body'][0].strip()
+        query = req.get_param('Body')
 
         if not query:
-            twil_resp.message(f"Please tell me the town and state you are in. For example, 'Anchorage, AK'")
-            resp.body = str(twil_resp)
+            resp.body = "Please tell me the town and state you are in. For example, 'Anchorage, AK'"
             return
 
         geodata = GeoData()
@@ -37,23 +29,21 @@ class LandResource(object):
         location = geodata.query_location(query)
 
         if location is None:
-            twil_resp.message(f"I could not find the location: {query}")
-            resp.body = str(twil_resp)
+            resp.body = f"I could not find the location: {query}"
             return
 
         r = geodata.native_land_from_point(location['latitude'], location['longitude'])
 
         if r is None:
-            twil_resp.message(f"Sorry, I could not find anything about {location['city']}, {location['state']}")
-            resp.body = str(twil_resp)
+            resp.body = f"Sorry, I could not find anything about {location['city']}, {location['state']}"
             return
 
-        twil_resp.message(f"In {location['city']}, {location['state']} you are on {r['name']} land.")
-        resp.body = str(twil_resp)
+        resp.body = f"In {location['city']}, {location['state']} you are on {r['name']} land."
 
 
-app = falcon.API()
+app = falcon.API(media_type=falcon.MEDIA_XML)
 
+app.req_options.auto_parse_form_urlencoded = True
 resource = LandResource()
 
 app.add_route('/', resource)
